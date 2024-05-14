1a503b9c0e5c8ebaeceb4b2363da7300dd66e350
PieterNatanael <pieternatanael@icloud.com>
2024-05-14 12:11:45 +0800

    Initial Commit

---
+++ What Day? Watch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  What Day? Watch Watch App
+//
+//  Created by Pieter Yoshua Natanael on 14/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ What Day? Watch Watch App/What_Day__WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  What_Day__WatchApp.swift
+//  What Day? Watch Watch App
+//
+//  Created by Pieter Yoshua Natanael on 14/05/24.
+//
+
+import SwiftUI
+
+@main
+struct What_Day__Watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
